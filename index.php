478cd46e078447d088f46ad4df413b02f2bf8f37
Ralph Lopez <ralphlopez5030@gmail.com>
2019-10-23 16:42:33 +0800

    vanilla

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+  <link rel="stylesheet" href="vendor/css/bootstrap.css">
+  <link rel="stylesheet" href="resource/css/login.css">
+  <link href="vendor/fonts/css/fontawesome.css" rel="stylesheet">
+  <link href="vendor/fonts/css/brands.css" rel="stylesheet">
+  <link href="vendor/fonts/css/solid.css" rel="stylesheet">
+</head>
+<body>
+  <!-- start here -->
+</body>
+</html>
+  <script src="vendor/js/jquery.min.js"></script>
+  <script src="vendor/js/popper.min.js"></script>
+  <script src="vendor/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js" integrity="sha256-jGAkJO3hvqIDc4nIY1sfh/FPbV+UK+1N+xJJg6zzr7A=" crossorigin="anonymous"></script>
+  <script src="vendor/js/main.js"></script>
